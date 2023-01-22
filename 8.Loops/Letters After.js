33e9d5039dffae7629af2ee2cc575eaf39b533d5
qw3r1nt1n3 <joseph.aviles.dalton@gmail.com>
2023-01-21 23:58:24 -0600

    8.Loops

---
+++ 8.Loops/Letters After.js
@@ -0,0 +1,42 @@
+// My Code
+
+function lettersAfter (haystack, needle, limit) {
+    let letters = ''
+        for (let x = 0; x < haystack.length; x++){
+            if (haystack[x] === needle){
+            for (let y = 0; y < limit; y++){
+                let innerChar = haystack[x + y + 1]
+                letters += innerChar;
+                }
+                break
+            }
+        }
+    return letters
+    }
+
+console.log(lettersAfter('any value', 'a', 2))
+
+
+
+// chatgpt example code
+
+// function lettersAfter(haystack, needle, limit) {
+//   let needleFound = false;
+//   let result = "";
+//   for (let i = 0; i < haystack.length; i++) {
+//     if (haystack[i] === needle) {
+//       needleFound = true;
+//       for (let j = i + 1; j <= i + limit && j < haystack.length; j++) {
+//         result += haystack[j];
+//       }
+//       break;
+//     }
+//   }
+//   if (!needleFound) {
+//     return "";
+//   }
+//   return result;
+// }
+
+// console.log(lettersAfter('hello', 'e', 2))
+
